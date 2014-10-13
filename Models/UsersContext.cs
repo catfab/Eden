c906b697bc1049c3395682e832823e6b719af3bc
Fabrizio Cattaneo <fabrizio.cattaneo@esprinet.com>
2014-10-13 21:28:27 +0200

    Inserisci prima commit

---
+++ Models/UsersContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Eden.Models
+{
+    using System.Data.Entity;
+
+    public class UsersContext : DbContext
+    {
+        public UsersContext()
+            : base("Eden")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
